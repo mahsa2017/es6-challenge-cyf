bb08c7e6e89008f5142b626a131392079e78e5b1
mahsa2017 <upmahjour@gmail.com>
2018-02-27 15:58:30 +0000

    conversion to es6 done

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 // Turn this function into an arrow function
-function arrow() {
+/* function arrow() {
   return 'es6 is awesome!'
-}
+} */
+const arrow = () => 'es6 is awesome!';
 
 //Use the es6 syntax for default parameter
-function defaultParameter(name) {
+/* function defaultParameter(name) {
   var name = name || 'sam'
   return name
-}
+} */
+const defaultParameter = (name) => (name) ? name : 'sam'; //????????????? correct ?
 
 // Use the spread operator to combine arr1 and arr2
-function combineArrays(arr1, arr2) {
+/* function combineArrays(arr1, arr2) {
   return arr1.concat(arr2)
-}
+} */
+const combineArrays = (arr1, arr2) => arr1.concat(arr2);
 
 //use destructuring to return the object's cyf property
-function destructuring(obj) {
+/* function destructuring(obj) {
   return obj.cyf
-}
+} */
+const destructuring = obj => obj.cyf;
 
 // use template literal to return a string with the sum of a and b
-function templateString(a, b) {
+/* function templateString(a, b) {
   return 'The sum is equal to ' + (a + b).toString()
-}
-
+} */
+const templateString=(a,b)=>`The sum is equal to ${a+b}`; //?????????????? correct ?
 
 module.exports = {
   arrow,
